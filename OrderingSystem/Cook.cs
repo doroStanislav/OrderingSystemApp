0dabb847352880358440d643a9b1d845a3f6c7b7
stasiqd <159320211+stasiqd@users.noreply.github.com>
2025-02-13 21:19:10 +0200

    added functionality for the Cook class

--- OrderingSystem/Cook.cs
+++ OrderingSystem/Cook.cs
@@ -3,10 +3,31 @@
     public class Cook
     {
         public string Name { get; set; }
+        public List<Dish> DishesInProgress { get; set; }
 
         public Cook(string name)
         {
             Name = name;
+            DishesInProgress = new List<Dish>();
+        }
+
+        public bool CanTakeOrder()
+        {
+           return DishesInProgress.Count < 5;
+        }
+
+        public void AddDish(Dish dish)
+        {
+            DishesInProgress.Add(dish);
+        }
+        public void RemoveDish(Dish dish)
+        {
+            DishesInProgress.Remove(dish);
+        }
+
+        public int GetTotalCookingTime()
+        {
+            return DishesInProgress.Sum(d => d.EstimatedCookingTime);
         }
     }
 }
