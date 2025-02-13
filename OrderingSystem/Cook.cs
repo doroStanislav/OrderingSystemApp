cbf91308bbb0d8326a572202c8ed932484bae44d
stasiqd <159320211+stasiqd@users.noreply.github.com>
2025-02-13 19:57:05 +0200

    Add project files.

---
+++ OrderingSystem/Cook.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderingSystem
+{
+    public class Cook
+    {
+        public string Name { get; set; }
+
+        public Cook(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ OrderingSystem/Dish.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystem
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public int EstimatedCookingTime { get; set; }
+
+        public Dish(string name, string description, string price, int estimatedCookingTime)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            EstimatedCookingTime = estimatedCookingTime;
+        }
+    }
+}
